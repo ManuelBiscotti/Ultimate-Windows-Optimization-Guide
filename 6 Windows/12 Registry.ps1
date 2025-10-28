6d0f7e1564761bdb91145e156bca5931c848886e
ManuelTest <manueltesting01@gmail.com>
2025-10-28 22:01:01 +0100

    fixes

--- 6 Windows/12 Registry.ps1
+++ 6 Windows/12 Registry.ps1
@@ -2576,6 +2576,42 @@ E0,F6,C5,D5,0E,CA,50,00,00
 ; disable windows search indexing
 [HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\WSearch]
 "Start"=dword:00000004
+
+
+
+
+; D3D11 - D3D12 Tweaks
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\DirectX]
+"D3D12_ENABLE_UNSAFE_COMMAND_BUFFER_REUSE"=dword:00000001
+"D3D12_ENABLE_RUNTIME_DRIVER_OPTIMIZATIONS"=dword:00000001
+"D3D12_RESOURCE_ALIGNMENT"=dword:00000001
+"D3D11_MULTITHREADED"=dword:00000001
+"D3D12_MULTITHREADED"=dword:00000001
+"D3D11_DEFERRED_CONTEXTS"=dword:00000001
+"D3D12_DEFERRED_CONTEXTS"=dword:00000001
+"D3D11_ALLOW_TILING"=dword:00000001
+"D3D11_ENABLE_DYNAMIC_CODEGEN"=dword:00000001
+"D3D12_ALLOW_TILING"=dword:00000001
+"D3D12_CPU_PAGE_TABLE_ENABLED"=dword:00000001
+"D3D12_HEAP_SERIALIZATION_ENABLED"=dword:00000001
+"D3D12_MAP_HEAP_ALLOCATIONS"=dword:00000001
+"D3D12_RESIDENCY_MANAGEMENT_ENABLED"=dword:00000001
+
+; DirectX Driver DXGKrnl Advanced Tweaks (2)
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DXGKrnl]
+"CreateGdiPrimaryOnSlaveGPU"=dword:00000001
+"DriverSupportsCddDwmInterop"=dword:00000001
+"DxgkCddSyncDxAccess"=dword:00000001
+"DxgkCddSyncGPUAccess"=dword:00000001
+"DxgkCddWaitForVerticalBlankEvent"=dword:00000001
+"DxgkCreateSwapChain"=dword:00000001
+"DxgkFreeGpuVirtualAddress"=dword:00000001
+"DxgkOpenSwapChain"=dword:00000001
+"DxgkShareSwapChainObject"=dword:00000001
+"DxgkWaitForVerticalBlankEvent"=dword:00000001
+"DxgkWaitForVerticalBlankEvent2"=dword:00000001
+"SwapChainBackBuffer"=dword:00000001
+"TdrResetFromTimeoutAsync"=dword:00000001
 "@
 
 				Set-Content -Path "$env:TEMP\Registry Optimize.reg" -Value $MultilineComment -Force
